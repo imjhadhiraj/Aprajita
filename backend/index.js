c8cf730d3904783422d7c0768972bb2ae4ed94c5
Kshitij Kumar <kumarkshitij171@gmail.com>
2024-08-10 16:09:46 +0530

    setting up backend route

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/api', (req, res) => {
+    res.json({ message: 'Welcome to the API' });
 });
 
 app.listen(PORT, () => {
